28a0c154a075b8b793ac12555a43c0e90f504bab
Amarnath reddy <amar.vallapreddy@gmail.com>
2022-04-04 13:20:06 +0530

    added new consturctor with single parameter

--- Learn.java
+++ Learn.java
@@ -2,6 +2,10 @@ public class Learn {
     int id;
     String name;
 
+    Learn(int id) {
+        this.id = id;
+    }
+
     Learn(int id, String name) {
         this.id = id;
         this.name = name;
